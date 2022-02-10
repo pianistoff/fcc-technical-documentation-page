7551b4c9bf43e421c3d2120fd75147c0b7161866
Severyn Hundiak <severyn.h@gmail.com>
2022-02-10 20:41:43 +0200

    Add mediq query to scrollSpy

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 let section = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll(".nav-link");
+const mq = window.matchMedia("(min-width: 962px)");
 
 window.onscroll = () => {
-    section.forEach((sec) => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute("id");
+    if (mq.matches) {
+        section.forEach((sec) => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute("id");
+            console.log(top);
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach((links) => {
-                links.classList.remove("active");
-                document
-                    .querySelector("nav a[href*=" + CSS.escape(id) + "]")
-                    .classList.add("active");
-            });
-        }
-    });
+            if (top >= offset && top < offset + height) {
+                navLinks.forEach((links) => {
+                    links.classList.remove("active");
+                    document
+                        .querySelector("nav a[href*=" + CSS.escape(id) + "]")
+                        .classList.add("active");
+                });
+            }
+        });
+    }
 };
