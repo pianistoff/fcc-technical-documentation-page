7946730eca83737f80a46b8b7b0e84aa81ecb1aa
Severyn Hundiak <severyn.h@gmail.com>
2022-02-10 20:43:19 +0200

    Remove useless console.log

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ window.onscroll = () => {
             let offset = sec.offsetTop - 150;
             let height = sec.offsetHeight;
             let id = sec.getAttribute("id");
-            console.log(top);
 
             if (top >= offset && top < offset + height) {
                 navLinks.forEach((links) => {
